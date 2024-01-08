bc1e1a52fc87ca2a1084019b81881b6132952011
JonathanF123 <jonathanlevid3@gmail.com>
2024-01-08 16:02:59 +0700

    membuat KasirSeeder

--- project-akhir-web-perjuangan/database/seeders/DatabaseSeeder.php
+++ project-akhir-web-perjuangan/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Supplier::factory(10)->create();
         $this->call(BarangSeeder::class);
-
+        $this->call(KasirSeeder::class);
     }
 }

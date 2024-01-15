9b35aba9e479a4de0f76f80accc02e1a2ba246f7
Richie Jonathan Chaniago <richiejonathanchaniago@gmail.com>
2024-01-15 11:15:44 +0700

    fix seeder

--- project-akhir-web-perjuangan/database/seeders/TransaksiPenjualanSeeder.php
+++ project-akhir-web-perjuangan/database/seeders/TransaksiPenjualanSeeder.php
@@ -20,7 +20,7 @@ class TransaksiPenjualanSeeder extends Seeder
                 'tgl_transaksi' => '2024-01-02'
             ],
             [
-                'User_id' => '1',
+                'User_id' => '2',
                 'sub_total' => '3000',
                 'tgl_transaksi' => '2024-01-03'
             ],
@@ -30,27 +30,27 @@ class TransaksiPenjualanSeeder extends Seeder
                 'tgl_transaksi' => '2024-01-03'
             ],
             [
-                'User_id' => '1',
+                'User_id' => '2',
                 'sub_total' => '3000',
                 'tgl_transaksi' => '2024-01-03'
             ],
             [
-                'User_id' => '1',
+                'User_id' => '2',
                 'sub_total' => '46000',
                 'tgl_transaksi' => '2024-01-03'
             ],
             [
-                'User_id' => '1',
+                'User_id' => '3',
                 'sub_total' => '33000',
                 'tgl_transaksi' => '2024-01-03'
             ],
             [
-                'User_id' => '1',
+                'User_id' => '3',
                 'sub_total' => '32000',
                 'tgl_transaksi' => '2024-01-03'
             ],
             [
-                'User_id' => '1',
+                'User_id' => '2',
                 'sub_total' => '31000',
                 'tgl_transaksi' => '2024-01-03'
             ],
